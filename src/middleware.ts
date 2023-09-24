19b2f8e3fee76f32905f35e186f6c6852c81e49b
Jeanpierre Rea <0703943+jeanpierrerea@users.noreply.github.com>
2023-09-24 17:19:14 -0400

    Routers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,11 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+import { withClerkMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
-export default authMiddleware({ publicRoutes: ['/', '/api/webhook/clerk'], ignoredRoutes: ['/api/webhook/clerk'], });
- 
+export default withClerkMiddleware(() => {
+      return NextResponse.next();
+});
+
+
 export const config = {
-      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+      matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
 };
- 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,9 +17,11 @@ import {
 } from "@clerk/nextjs";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
+
   return (
     <>
       <Head>
@@ -34,6 +36,13 @@ export default function Home() {
 
         {!user.isSignedIn && <SignInButton />}
         {!!user.isSignedIn && <SignOutButton />}
+
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
+
 
         <UserButton />
 
