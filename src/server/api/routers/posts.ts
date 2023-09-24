d4937efc6250dd9080885a99f4f1f8a985d196c1
Jeanpierre Rea <0703943+jeanpierrerea@users.noreply.github.com>
2023-09-24 19:39:26 -0400

    Created posts & associating with users

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,32 @@
+import { clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
+import type { User } from "@clerk/nextjs/server";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+
+    return {id: user.id, 
+        username: user.username, 
+        profilePicture: user.imageUrl,
+    };
+}
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+        take: 100,
+    });
+
+    const users = (await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+    })
+    ).map(filterUserForClient);
+
+    return posts.map(post => ( {
+        post,
+        author: users.find((user) => user.id === post.authorId),
+    }));
   }),
 });
